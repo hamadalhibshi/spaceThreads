bb7eb1249314749f29dc1894ef0b1fdaddb87fc5
Qasim <alkuwaitiqasim@gmail.com>
2023-09-10 15:18:10 +0300

    added a feature for the list of stories

---
+++ django-api/api/story/serializers.py
@@ -0,0 +1,43 @@
+from api.story.models import Story, Reply, Review, Chapter, Comment
+from rest_framework import serializers
+
+
+class StorySerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Story
+        fields = '__all__'
+        read_only_field = ["id"]
+
+class ReplySerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Reply
+        fields = '__all__'
+        read_only_field = ["id"]
+
+class ReviewSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_field = ["id"]
+
+class ChapterSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Chapter
+        fields = '__all__'
+        read_only_field = ["id"]
+
+class CommentSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_field = ["id"]
