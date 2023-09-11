b75113901d427dc2b6069bf084ef1d6585612131
Ahmed Almadhoob <ahmed.almadhoob@souqalmanama.com>
2023-09-11 10:21:25 +0300

    Combined views.py with story/views.py

--- django-api/api/story/views.py
+++ django-api/api/story/views.py
@@ -5,7 +5,8 @@ from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.exceptions import ValidationError
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
 def listStories(request):
@@ -16,3 +17,47 @@ def listStories(request):
         return JsonResponse(data, safe=False)
     else:
         raise ValidationError("Method not allowed")
+
+
+@api_view(['GET'])
+def storyDetails(request, story_id):
+    try:
+        # Retrieve the story instance based on the provided story_id
+        story = Story.objects.get(id=story_id)
+
+        # Serialize the story instance
+        serializer = StorySerializer(story)
+
+        # Return the serialized data as a JSON response
+        return JsonResponse(serializer.data)
+    
+    except Story.DoesNotExist:
+        # Handle the case where the story with the provided ID does not exist
+        return JsonResponse({'error': 'Story not found'}, status=404)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createStory(request):
+    # Access the logged-in user's ID using request.user
+    user_id = request.user.id
+
+    # Get the data from the request
+    data = request.data
+
+    # Add the user ID to the data as authorId
+    data['authorId'] = user_id
+
+    # Create a new Story object and save it to the database
+    try:
+        story = Story.objects.create(**data)
+
+        # Serialize the created story
+        serializer = StorySerializer(story)
+
+        # Return a success response
+        return JsonResponse({'message': 'Story created successfully', 'story': serializer.data}, status=201)
+    
+    except Exception as e:
+        # Handle any exceptions (e.g., validation errors)
+        return JsonResponse({'error': str(e)}, status=400)
