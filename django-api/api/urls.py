89be4fa08050f38fbf548a00de4eda63d716b50f
Qasim <alkuwaitiqasim@gmail.com>
2023-09-11 10:11:21 +0300

    added details view

--- django-api/api/urls.py
+++ django-api/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('storyDetails/<int:story_id>/', views.storyDetails, name='story_details'),
+    path('createStory/', views.createStory, name='create_story'),
+
 ]
