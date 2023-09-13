a372b242f38ac97f20b69bfd10426d9ecc744cb3
Ali Haji <ali-rider@live.com>
2023-09-13 16:06:46 +0300

    Comments section

--- react-ui/src/pages/LandingPages/StoriesRead/index.js
+++ react-ui/src/pages/LandingPages/StoriesRead/index.js
@@ -20,6 +20,8 @@ import { Divider, Icon, IconButton } from "@mui/material";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { FormControl, InputLabel, Select, MenuItem, styled } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { Paper, Typography } from "@mui/material";
 
 function StoriesRead() {
   const popularFontFamilies = [
@@ -86,6 +88,17 @@ function StoriesRead() {
     setSelectedFont(event.target.value);
     console.log(selectedFont);
   };
+
+  // Styled components
+  const CommentContainer = styled(Paper)`
+    && {
+      margin-bottom: 16px;
+    }
+  `;
+
+  const CommentText = styled("div")`
+    padding-left: ${({ isReply }) => (isReply ? "32px" : "0")};
+  `;
 
   return (
     <>
@@ -269,7 +282,7 @@ function StoriesRead() {
           textJustify="inter-word"
           opacity={0.8}
           mt={1}
-          mb={3}
+          mb={5}
         >
           Zorian’s eyes abruptly shot open as a sharp pain erupted from his stomach. His whole body
           convulsed, buckling against the object that fell on him, and suddenly he was wide awake,
@@ -300,6 +313,95 @@ function StoriesRead() {
           waiting till she dropped her guard. Predictably, Kirielle grew visibly agitated after only
           a few moments of this pretend disinterest.
         </MKTypography>
+      </Card>
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        <Divider sx={{ bgcolor: "secondary.dark" }} />
+        <MKTypography
+          variant="h4"
+          sx={{ mb: 5 }}
+          component="span"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-around"
+        >
+          <Grid container item xs={12} lg={8} justifyContent="space-between">
+            <MKButton component={Link} to={"/home"} variant="gradient" size="small" color={"dark"}>
+              Previous Chapter
+            </MKButton>
+            <MKButton
+              component={Link}
+              to={"/stories/show"}
+              variant="gradient"
+              size="small"
+              color={"dark"}
+            >
+              Chapters Index
+            </MKButton>
+            <MKButton component={Link} to={"/home"} variant="gradient" size="small" color={"dark"}>
+              Next Chapter
+            </MKButton>
+          </Grid>
+        </MKTypography>
+        <Divider sx={{ bgcolor: "secondary.dark" }} />
+        <MKTypography variant="h4" sx={{ mb: 5 }} component="span" alignItems="left">
+          Comments (*Comments number*)
+        </MKTypography>
+
+        <Grid container item justifyContent="center" alignItems="center">
+          <Grid item xs={12} lg={8}>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="prologue"
+              name="prologue"
+              label="Write a Comment"
+              autoComplete="off"
+              variant="outlined"
+              multiline
+              rows={2}
+            />
+          </Grid>
+          <Grid item xs={12} lg={8}>
+            <MKButton component={Link} to={"/home"} variant="gradient" size="small" color={"dark"}>
+              Add Comment
+            </MKButton>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} lg={8}>
+          {/* Comment 1 */}
+
+          <MKTypography sx={{ mt: 4 }} variant="h6">
+            Commentor Name
+          </MKTypography>
+          <CommentText>
+            <MKTypography variant="body1">This is the comment text for Comment 1.</MKTypography>
+          </CommentText>
+          <MKButton
+            component={Link}
+            to={"/home"}
+            variant="gradient"
+            size="small"
+            color={"dark"}
+            sx={{ marginLeft: "auto", justifyContent: "space-between" }}
+          >
+            Reply
+          </MKButton>
+          {/* Reply to Comment 1 */}
+          <Grid item xs={12} lg={8} sx={{ ml: 8, mt: 2 }}>
+            <MKTypography variant="h6">Replier Name</MKTypography>
+            <CommentText>
+              <MKTypography variant="body1">This is the reply to Comment 1.</MKTypography>
+            </CommentText>
+          </Grid>
+        </Grid>
       </Card>
       <MKBox pt={6} px={1} mt={6}>
         <DefaultFooter content={footerRoutes} />
