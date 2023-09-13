32a3b740563081e688309b0f9bc270c702c88781
Ahmed Almadhoob <ahmed.almadhoob@souqalmanama.com>
2023-09-13 10:36:34 +0300

    Fix admin issue

--- django-api/core/urls.py
+++ django-api/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.contrib import admin
 from api import urls as api_urls
 
 urlpatterns = [
+    # path('admin/', admin.site.urls).
     path("api/users/", include(("api.routers", "api"), namespace="api")),
     path('api/', include(api_urls)),
 ]
