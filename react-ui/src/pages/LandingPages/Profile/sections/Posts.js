68a569d82c194203ab3354dfe58e94d73bd320de
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-09-18 14:06:30 +0300

    fixed styling for profile

--- react-ui/src/pages/LandingPages/Profile/sections/Posts.js
+++ react-ui/src/pages/LandingPages/Profile/sections/Posts.js
@@ -37,6 +37,15 @@ import StoryApi from "../../../../api/story";
 
 
 
+function truncateText(text, maxLength) {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  const truncatedText = text.substr(0, maxLength);
+  return `${truncatedText.trim()}...`;
+}
+
+
 function Posts() {
   const { user } = useAuth();
   const [data, setData] = useState();
@@ -75,17 +84,16 @@ function Posts() {
           </MKTypography>
         </Grid>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} lg={3}>
-            {data &&
-              data.data.stories_with_reviews.map((item, index) => {
-                const imageUrl = item.story.image.replace("image/upload/", "");
-
-                return (
+          {data &&
+            data.data.stories_with_reviews.map((item, index) => {
+              const imageUrl = item.story.image.replace("image/upload/", "");
+              const truncatedDescription = truncateText(item.story.prologue, 100);
+              return (
+                <Grid item xs={12} sm={6} lg={3} key={index}>
                   <TransparentBlogCard
-                    key={index}
                     image={imageUrl}
                     title={item.story.title}
-                    description="Finding temporary housing for your dog should be as easy as renting an Airbnb. That's the idea behind Rover ..."
+                    description={truncatedDescription}
                     action={{
                       type: "internal",
                       route: "/pages/blogs/author",
@@ -93,50 +101,9 @@ function Posts() {
                       label: "read more",
                     }}
                   />
-                );
-              })}
-
-
-          </Grid>
-
-          {/* <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post2}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "/pages/blogs/author",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "/pages/blogs/author",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <BackgroundBlogCard
-              image={post4}
-              title="Flexible work hours"
-              description="Rather than worrying about switching offices every couple years, you stay in the same place."
-              action={{
-                type: "internal",
-                route: "/pages/blogs/author",
-                label: "read more",
-              }}
-            />
-          </Grid> */}
+                </Grid>
+              );
+            })}
         </Grid>
       </Container>
     </MKBox>
